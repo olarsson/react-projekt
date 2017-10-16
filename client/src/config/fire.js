dbaf00bc73f7e49782d9c2c68ed0c6690dd334a6
folofolo <olarsson@live.com>
2017-10-16 17:18:38 +0200

    Heroku

---
+++ client/src/config/fire.js
@@ -0,0 +1,13 @@
+//import firebase from 'firebase'
+var firebase = require('firebase');
+var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+  apiKey: "AIzaSyAPyfo5lf9472xFGK0dhiJVUxkeqtoyWSo",
+  authDomain: "project-test-ab10f.firebaseapp.com",
+  databaseURL: "https://project-test-ab10f.firebaseio.com",
+  projectId: "project-test-ab10f",
+  storageBucket: "",
+  messagingSenderId: "452972685925"
+};
+var fire = firebase.initializeApp(config);
+//export default fire;
+module.exports = fire;
